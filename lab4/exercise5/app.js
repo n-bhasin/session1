21c4566ce1587112aafa74ea3e4d590c3c8215f2
bhasineeraj <nkbhasin28@gmail.com>
2020-03-26 19:10:52 -0400

    exercise5

--- lab4/exercise5/app.js
+++ lab4/exercise5/app.js
@@ -1,11 +1,21 @@
 const http = require('http');
+const url = require('url');
 const hostname = '127.0.0.1';
 const port = 3000;
+
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    // res.end('Hello world\n');
+    if(req.url === '/spin'){
+        console.log('spinning')
+        res.end('Slot Machine')
+    }
+    if(req.url === '/play'){
+        console.log('playing')
+        res.end('Slot Machine')
+    }
 });
 
 server.listen(port, hostname, ()=> {
