1e2ffd29f88f673e02edb2c2a66ec773e65960a6
bhasineeraj <nkbhasin28@gmail.com>
2020-03-26 20:48:50 -0400

    part2

--- lab4/exercise5/app.js
+++ lab4/exercise5/app.js
@@ -1,15 +1,25 @@
 const http = require('http');
 const url = require('url');
 const hostname = '127.0.0.1';
-const port = 3000;
+const port = 4000;
 
 
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
     // res.end('Hello world\n');
+    // let URL = url.parse(req.url,true); 
+    let abc = url.parse(req.url,true).query; 
+    var amount;
     if(req.url === '/spin'){
         console.log('spinning')
+        console.log('amount lost:' + amount);
+        res.end('Slot Machine')
+    }
+
+    if(abc.amount){  
+        amount = abc.amount;
+        console.log('playing....amount:' + abc.amount)
         res.end('Slot Machine')
     }
     if(req.url === '/play'){
